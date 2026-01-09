82c2940927b5bd7fa1c621f243d0e94d9e687627
will_code <will.tech.code@gmail.com>
2026-01-09 18:40:34 -0500

    switch to pool

--- index.js
+++ index.js
@@ -1,26 +1,20 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const PORT = process.env.PORT || process.env.PORT || 3000;
-const { Client } = require('pg');
+const PORT = process.env.PORT  || 3000;
+const { Pool } = require('pg');
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
+const { Pool } = require("pg");
 
-const connectionString = process.env.URL_DATABASE;
-
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
+const pool = new Pool({
+  connectionString: process.env.URL_DATABASE,
   ssl: { rejectUnauthorized: false }
 });
 
-
-client.connect()
-  .then(() => console.log('Conectado a PostgreSQL'))
-  .catch(err => console.error('Error de conexión:', err));
- 
 
 app.get('/', (req, res) => {
   res.send('¡Hola, API en Express funcionando!');
@@ -29,13 +23,24 @@ app.get('/', (req, res) => {
 // Ruta para obtener todas las cobranzas    
 app.get("/cobranzas", async (req, res) => {
   try {
-    const result = await client.query("SELECT * FROM cobranzas");
+    const result = await pool.query("SELECT * FROM cobranzas");
     res.status(200).json(result.rows);
   } catch (err) {
     console.error("ERROR DB:", err);
     res.status(500).json({ error: "DB error" });
   }
 });
+
+app.get("/test-db", async (req, res) => {
+  try {
+    const r = await pool.query("SELECT 1");
+    res.json({ ok: true });
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ ok: false });
+  }
+});
+
 
 
 
