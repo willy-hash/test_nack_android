c5d5f39e8430f659aa1b9f22c8a09158853ec621
will_code <will.tech.code@gmail.com>
2026-01-09 19:31:57 -0500

    swicth postgres pk

--- index.js
+++ index.js
@@ -1,50 +1,56 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const PORT = process.env.PORT  || 3000;
-const { Pool } = require('pg');
+const PORT = process.env.PORT || 3000;
+//const { Pool } = require('pg');
+const postgres = require('postgres');
+
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
 
-const pool = new Pool({
-  connectionString: process.env.URL_DATABASE,
-  ssl: { rejectUnauthorized: false }
+const connectionString = "postgresql://postgres:5qRM6c0aiYWwOsZ8@db.xsykxfkehlbnlcpidncu.supabase.co:5432/postgres"
+const sql = postgres(connectionString, {
+  ssl: 'require'
 });
 
 
+
+
+
 app.get('/', (req, res) => {
-  res.send('¡Hola, API en Express funcionando!');
+    res.send('¡Hola, API en Express funcionando!');
 });
 
 // Ruta para obtener todas las cobranzas    
 app.get("/cobranzas", async (req, res) => {
-  try {
-    const result = await pool.query("SELECT * FROM cobranzas");
-    res.status(200).json(result.rows);
-  } catch (err) {
-    console.error("ERROR DB:", err);
-    res.status(500).json({ error: "DB error" });
-  }
+    try {
+        const result = await sql`SELECT * FROM cobranzas`;
+        res.status(200).json(result);
+    } catch (err) {
+        console.error("ERROR DB:", err);
+        res.status(500).json({ error: "DB error" });
+    }
 });
 
 app.get("/test-db", async (req, res) => {
-  try {
-    const r = await pool.query("SELECT 1");
-    res.json({ ok: true });
-  } catch (e) {
-    console.error(e);
-    res.status(500).json({ ok: false });
-  }
+    try {
+        const r = await sql`SELECT 1`;
+        res.json({ ok: true });
+    } catch (e) {
+        console.error(e);
+        res.status(500).json({ ok: false });
+    }
 });
 
 
+console.log("DB:", process.env.URL_DATABASE);
 
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
 
 
